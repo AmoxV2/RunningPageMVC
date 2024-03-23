a12879f9e491832377fa5a3947d3acc2a6c55c76
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-03-23 23:24:07 +0100

    Repaired

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Learning_MVC.Models;
+
+namespace Learning_MVC.ViewModels
+{
+    
+        public class HomeViewModel
+        {
+            public IEnumerable<Club>? Clubs { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+          
+        }
+    
+}
